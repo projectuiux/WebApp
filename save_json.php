e338e6838b40833ee470b51255dde994ab062688
projectuiux <53488407+projectuiux@users.noreply.github.com>
2019-08-05 09:14:20 -0600

    save_json.php

--- save_json.php
+++ save_json.php
@@ -1,7 +1,7 @@
 <?php
     $myFile = "./api/data.json";
     $fh = fopen($myFile, 'w') or die("can't open file");
-    $stringData = $_GET["data"];
+    $stringData = $_POST["data"];
     //print_r( $stringData );
     if( fwrite($fh, $stringData) ){
         echo 'Writing new file';
